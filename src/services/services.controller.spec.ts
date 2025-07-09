038af47e2b43ae55f52a3580dfa87ba82bc3dc8d
Vipul Sharma <vipul212@gmail.com>
2025-07-09 13:50:33 -0400

    add unit tests

--- src/services/services.controller.spec.ts
+++ src/services/services.controller.spec.ts
@@ -1,18 +1,103 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServicesController } from './services.controller';
+import { ServicesService } from './services.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ServicesController', () => {
   let controller: ServicesController;
+  let service: ServicesService;
+  const resultServicesFixture = [
+    {
+      id: '1',
+      name: 'Service 1',
+      description: 'Test Service 1',
+      versions: ['v1.0.0'],
+    },
+    {
+      id: '2',
+      name: 'Service 2',
+      description: 'Test Service 2',
+      versions: ['v0.0.1'],
+    },
+  ];
+  const resultVersionsFixture = [
+    {
+      id: '1',
+      version: 'v1.0.0',
+      description: 'Version 1.0.0 of Service 1',
+      serviceId: '1',
+    },
+    {
+      id: '2',
+      version: 'v0.0.1',
+      description: 'Version 0.0.1 of Service 2',
+      serviceId: '2',
+    },
+  ];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [ServicesController],
+      providers: [
+        {
+          provide: ServicesService,
+          useValue: {
+            getAll: jest.fn().mockResolvedValue(resultServicesFixture),
+            getById: jest.fn().mockImplementation((id: number) => {
+              return resultServicesFixture[id - 1];
+            }),
+            getVersions: jest.fn().mockImplementation((id: number) => {
+              return resultVersionsFixture.filter((version) => version.serviceId === String(id));
+            }),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<ServicesController>(ServicesController);
+    controller = moduleRef.get<ServicesController>(ServicesController);
+    service = moduleRef.get<ServicesService>(ServicesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getAllServices', () => {
+    it('should return an array of services', async () => {
+      expect(await controller.getAllServices({})).toEqual(resultServicesFixture);
+    });
+    it('should return an empty array when no services are found', async () => {
+      jest.spyOn(service, 'getAll').mockResolvedValue([]);
+      expect(await controller.getAllServices({})).toEqual([]);
+    });
+  });
+
+  describe('getServiceById', () => {
+    it('should return a service by ID', async () => {
+      const serviceId = 1;
+      expect(await controller.getServiceById(serviceId)).toEqual(resultServicesFixture[serviceId - 1]);
+    });
+
+    it('should throw NotFoundException for non-existent service ID', async () => {
+      const serviceId = 9999;
+      jest.spyOn(service, 'getById').mockImplementation(() => {
+        throw new NotFoundException(`Service with id ${serviceId} not found`);
+      });
+      await expect(controller.getServiceById(serviceId)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('getServiceVersions', () => {
+    it('should return versions of a service by ID', async () => {
+      const serviceId = 1;
+      expect(await controller.getServiceVersions(serviceId)).toEqual(
+        resultVersionsFixture.filter((version) => version.serviceId === String(serviceId)),
+      );
+    });
+
+    it('should throw NotFoundException for non-existent service ID', async () => {
+      const serviceId = 9999;
+      jest.spyOn(service, 'getVersions').mockImplementation(() => {
+        throw new NotFoundException(`Service with id ${serviceId} not found`);
+      });
+      await expect(controller.getServiceVersions(serviceId)).rejects.toThrow(NotFoundException);
+    });
   });
 });
--- src/services/services.service.spec.ts
+++ src/services/services.service.spec.ts
@@ -1,18 +1,122 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServicesService } from './services.service';
+import { ServicesRepository } from './services.repository';
+import { describe } from 'node:test';
 
 describe('ServicesService', () => {
   let service: ServicesService;
+  let repository: ServicesRepository;
+  const mockServices = [
+    {
+      id: 1,
+      name: 'Service 1',
+      description: 'Test Service 1',
+      versions: [],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      id: 2,
+      name: 'Service 2',
+      description: 'Test Service 2',
+      versions: [],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ];
+  const mockVersions = [
+    {
+      id: 1,
+      description: 'Version 1.0 of Service 1',
+      version: '1.0',
+      service: mockServices[0],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      id: 2,
+      description: 'Version 1.1 of Service 1',
+      version: '1.1',
+      service: mockServices[0],
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ];
+  const mapper = (entity: { id: number | string; [key: string]: any }): any => {
+    return { ...entity, id: String(entity.id) };
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ServicesService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        ServicesService,
+        {
+          provide: ServicesRepository,
+          useValue: {
+            findAllServices: jest.fn().mockResolvedValue([]),
+            findServiceById: jest.fn().mockResolvedValue(null),
+            findAllVersionsForService: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<ServicesService>(ServicesService);
+    service = moduleRef.get<ServicesService>(ServicesService);
+    repository = moduleRef.get<ServicesRepository>(ServicesRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getAll', () => {
+    it('should return an empty array when no services are found', async () => {
+      const result = await service.getAll();
+      expect(result).toEqual([]);
+    });
+
+    it('should return a list of services', async () => {
+      jest.spyOn(repository, 'findAllServices').mockResolvedValue(mockServices);
+      const result = await service.getAll();
+      expect(result).toEqual(mockServices.map(mapper));
+    });
+  });
+
+  describe('getById', () => {
+    it('should throw NotFoundException when service is not found', async () => {
+      await expect(service.getById(9999)).rejects.toThrow('Service with id 9999 not found');
+    });
+
+    it('should return a service by ID', async () => {
+      jest.spyOn(repository, 'findServiceById').mockResolvedValue(mockServices[0]);
+      const result = await service.getById(1);
+      expect(result).toEqual(mapper(mockServices[0]));
+    });
+  });
+
+  describe('getVersions', () => {
+    it('should throw NotFoundException when service is not found', async () => {
+      jest.spyOn(repository, 'findAllVersionsForService').mockResolvedValue(null);
+      await expect(service.getVersions(9999)).rejects.toThrow('Service with id 9999 not found');
+    });
+
+    it('should not throw an error when service is found but has no versions', async () => {
+      jest.spyOn(repository, 'findAllVersionsForService').mockResolvedValue(mockServices[0].versions);
+      const result = await service.getVersions(1);
+      expect(result).toEqual([]);
+    });
+
+    it('should return versions of a service by ID', async () => {
+      jest.spyOn(repository, 'findAllVersionsForService').mockResolvedValue(mockVersions);
+      const result = await service.getVersions(1);
+      const mappedVersionRep = mockVersions.map((version) => {
+        return {
+          id: String(version.id),
+          description: version.description,
+          version: version.version,
+          created_at: version.created_at,
+          updated_at: version.updated_at,
+          serviceId: String(version.service.id),
+        };
+      });
+      expect(result).toEqual(mappedVersionRep);
+    });
   });
 });
